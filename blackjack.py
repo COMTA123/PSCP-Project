975c373848a3e948ddf3fc8206c11cfd919c1e5e
COMTA <thatchakornbunnin@gmail.com>
2025-09-23 23:19:13 +0700

    ใส่คอมเม้น

--- blackjack.py
+++ blackjack.py
@@ -11,17 +11,18 @@ def user_input():
     while amount_player <= 0 or amount_player > 3: #เช็คให้ไม่เกิน 3 และมากกว่า 0
         print("กรอกจำนวนผู้เล่นผิด")
         amount_player = int(input("กรอกใหม่อีกครั้ง"))
-    for _ in range(1,amount_player+1): #รับเงินที่คนจะเล่นโดย ตำแหน่งที่ 0 เป็นของบอท เราเลยเริ่มตำแหน่งที่ 1
+    for _ in range(1,amount_player+1): #loop ตามจำนวน Player
+        """รับเงินที่คนจะเล่นโดย ตำแหน่งที่ 0 เป็นของบอท เราเลยเริ่มตำแหน่งที่ 1"""
         start_money = 1000 #กำหนดเงินในบัญชีเริ่มต้นของทุกคน
         player_list.append([start_money])
     return amount_player #ให้ส่งจำนวนผู้เล่นออกมานอก function
 def insert_money(amount_player):
     """ฟังชันให้ผู้เล่นเลือกลงจำนวนเงิน"""
-    for i in range(1,amount_player+1): #loop ให้ทุก player
+    for i in range(1,amount_player+1): #loop ตามจำนวน Player
         money = int(input(f"Player[{i}] : ใส่จำนวนเงินที่จะลงในตานี้"))
         while money > player_list[i][0] or money <= 0: #เช็คว่าใส่เงินมากกว่าที่มีรึป่าว หรือ ใส่เงินน้อยกว่า 0 รึป่าว
             money = int(input("กรอกจำนวนเงินผิด"))
-        player_list[i].append(money)
+        player_list[i].append(money) #เพิ่ม money เข้าไปใน playerlist ในช่องplayer i
 
 def deal_cards():
     """randomCard 2 ใบ"""
@@ -30,45 +31,45 @@ def deal_cards():
 "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH",
 "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD",
 "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC"]
-    for i in range(1,amount_player+1):
-        random_card1 = random.sample(card_list,1)[0]
-        player_list[i].append(random_card1)
-        card_list.remove(random_card1)
-        random_card2 = random.sample(card_list,1)[0]
-        player_list[i].append(random_card2)
-        card_list.remove(random_card2)
-    print("--------------------------------------")
-    print(card_list)
-    print("--------------------------------------")
-    print(player_list)
+    for i in range(0,amount_player): #loop ตามจำนวน Player และบอทตำแหน่งที่ 0 ด้วย
+        random_card1 = random.sample(card_list,1)[0] #สุ่มการ์ดใบแรก
+        player_list[i].append(random_card1) #เพิ่มการ์ดเข้า playerlist
+        card_list.remove(random_card1)#ลบออกจากcardlist
+        random_card2 = random.sample(card_list,1)[0]#สุ่มใบสอง
+        player_list[i].append(random_card2)#เพิ่มการ์ด
+        card_list.remove(random_card2)#ลบออก
 
 def action():
     """Player Select Action"""
     print(card_list)
-    for i in range(1,amount_player+1):
-        player_action = str(input(f"เลือก Action player{i} : ")).lower()
-        player_action = check_action(player_action)
+    for i in range(1,amount_player+1): #loop ตาม player
+        player_action = str(input(f"เลือก Action player{i} : ")).lower() #รับinput action
+        player_action = check_action(player_action) #เข้าฟังชันเช็คว่า input ถูกไหม
         match player_action:
-            case "hit":
-                random_card_hit = random.sample(card_list,1)[0]
-                player_list[i].append(random_card_hit)
-                card_list.remove(random_card_hit)
-                player_action = str(input(f"เลือก Action player{i} : ")).lower()
-                player_action = check_action(player_action)
+            case "hit": #ถ้าเลือก Hit
+                random_card_hit = random.sample(card_list,1)[0] #สุ่มการ์ดเพิ่ม
+                player_list[i].append(random_card_hit) #เพิ่มการ์ดเข้าplayerlist
+                card_list.remove(random_card_hit) #ลบการออก card list
+                player_action = str(input(f"เลือก Action player{i} : ")).lower() #รับinputอีกครั้ง
+                player_action = check_action(player_action) #เข้าฟังชันเช็ค
                 while player_action == "hit":
+                    """ทำซ้ำไปเรื่อยๆหากจนกว่าจะไม่เลือก hit"""
                     random_card_hit = random.sample(card_list,1)[0]
                     player_list[i].append(random_card_hit)
                     card_list.remove(random_card_hit)
                     player_action = str(input(f"เลือก Action player{i} : ")).lower()
                     player_action = check_action(player_action)
             case "stand":
+                """ไม่ทำไรเลย"""
                 pass
         print(f"player{i} : {player_list[i]}")
 def check_action(act):
+    """เช็คว่าinput action ถูกไหม"""
     while act != "hit" and act != "stand":
         act = str(input("เลือก Action ไม่ถูกต้อง เลือกใหม่ : ")).lower()
     return act
 def check_score():
+    """ยังทำไม่เสร็จ"""
     for i in range(1,amount_player+1):
         for j in player_list[1][2:]:
             
